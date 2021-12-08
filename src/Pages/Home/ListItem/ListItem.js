accac983d5ed2aabe6ed9e60ee09ea10922d4489
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-12-09 01:41:48 +0600

    Update project

--- src/Pages/Home/ListItem/ListItem.js
+++ src/Pages/Home/ListItem/ListItem.js
@@ -1,10 +1,20 @@
+import { Add, PlayArrow, ThumbDownOffAltOutlined, ThumbUpOutlined } from '@mui/icons-material';
 import React from 'react';
 import './ListItem.scss';
 
 const ListItem = () => {
     return (
         <div className="list-item">
-            item
+            <div className="item-img">
+                <img src="https://images.hdqwalls.com/download/terminator-arnold-7q-1920x1080.jpg" alt="" />
+                <div className="item-actions">
+                <ThumbUpOutlined/>
+                <ThumbDownOffAltOutlined/>
+                <PlayArrow/>
+                <Add/>
+            </div>
+            </div>
+           
         </div>
     );
 };
